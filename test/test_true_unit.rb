dc2df20e01f7ecebda601738c156984783562a78
Ryan Sonnek <ryan@socialcast.com>
2010-12-28 23:36:15 -0600

    add basic execution and assertion syntax

--- test/test_true_unit.rb
+++ test/test_true_unit.rb
@@ -1,39 +1,60 @@
 require File.join(File.dirname(__FILE__), 'helper')
 
 class TestTrueUnit < Test::Unit::TestCase
+  @@fixtures = []
   def self.register(key, &block)
     attr_accessor key
     (class << self; self end).send(:define_method, key) do
+      @@fixtures << key
       result = yield
       instance_variable_set "@#{key}", result
       self
     end
   end
 
-  def self.with(scope = nil, &block)
+  def self.with(fixtures, &block)
+    yield
+  ensure
+    @@fixtures = []
+    @@execution = nil
+  end
+  def self.execute(message = nil, &block)
+    @@execution = message
     yield
   end
-
-  def self.when
+  def self.asserts(message = nil, &block)
+    @@assertion = message
+    result = yield
+    raise Test::Unit::AssertionFailedError.new(test_sentence) unless result
+  ensure
+    @@assertion = nil
+  end
+  def self.test_sentence
+    ['assert', @@assertion, 'when executing', @@execution, 'with', @@fixtures.join(' and ')].join(' ')
   end
 
+  # declare fixtures
   class User
+    def do_something
+      puts 'doing something'
+    end
   end
-  register :basic_user do
-    User.new
-  end
-
   class Blog
   end
-  register :basic_blog do
+  register :a_basic_user do
+    User.new
+  end
+  register :a_basic_blog do
     Blog.new
   end
 
-
-  #########################
-  
-  with basic_user.basic_blog do
-    puts @basic_user.inspect
-    puts @basic_blog.inspect
+  #write tests
+  with a_basic_user.a_basic_blog do
+    execute '@a_basic_user.do_something' do
+      @a_basic_user.do_something
+    end
+    asserts 'did something' do
+      false
+    end
   end
 end
