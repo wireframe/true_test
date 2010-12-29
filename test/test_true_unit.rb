90e8fefb6e813e34ab1018833b89c24c657e21a1
Ryan Sonnek <ryan@socialcast.com>
2010-12-29 10:49:38 -0600

    extract code to separate module

--- test/test_true_unit.rb
+++ test/test_true_unit.rb
@@ -1,41 +1,56 @@
 require File.join(File.dirname(__FILE__), 'helper')
 
-class TestTrueUnit < Test::Unit::TestCase
-  @@fixtures = []
-  def self.register(key, &block)
-    attr_accessor key
-    (class << self; self end).send(:define_method, key) do
-      @@fixtures << key
+module TrueUnit
+  module DSL
+    @@fixtures = []
+    def register_fixture(key, &block)
+      attr_accessor key
+      (class << self; self end).send(:define_method, key) do
+        @@fixtures << key
+        result = yield
+        instance_variable_set "@#{key}", result
+        self
+      end
+    end
+
+    def with(fixtures, &block)
+      yield
+    ensure
+      @@fixtures.each do |fixture|
+        instance_variable_set "@#{fixture}", nil
+      end
+      @@fixtures = []
+      @@execution = nil
+    end
+
+    def execute(description = nil, &block)
+      @@execution = description
+      yield
+    end
+
+    def asserts(description = nil, &block)
+      @@assertion = description
+      puts test_sentence
       result = yield
-      instance_variable_set "@#{key}", result
-      self
+      raise Test::Unit::AssertionFailedError.new(test_sentence) unless result
+    ensure
+      @@assertion = nil
+    end
+
+    def test_sentence
+      context = ['assert', @@assertion, 'when executing', @@execution]
+      context += ['with', @@fixtures.join(' and ')] if @@fixtures && @@fixtures.any?
+      context.join(' ')
     end
   end
 
-  def self.with(fixtures, &block)
-    yield
-  ensure
-    @@fixtures = []
-    @@execution = nil
-  end
-  def self.execute(message = nil, &block)
-    @@execution = message
-    yield
-  end
-  def self.asserts(message = nil, &block)
-    @@assertion = message
-    puts test_sentence
-    result = yield
-    raise Test::Unit::AssertionFailedError.new(test_sentence) unless result
-  ensure
-    @@assertion = nil
-  end
-  def self.test_sentence
-    context = ['assert', @@assertion, 'when executing', @@execution]
-    context += ['with', @@fixtures.join(' and ')] if @@fixtures && @@fixtures.any?
-    context.join(' ')
-  end
+  include TrueUnit::DSL
+end
 
+class TestTrueUnit < Test::Unit::TestCase
+  extend TrueUnit
+
+  puts self.methods.sort.inspect
   # declare fixtures
   class User
     def do_something
@@ -47,10 +62,10 @@ class TestTrueUnit < Test::Unit::TestCase
   end
   class Blog
   end
-  register :a_basic_user do
+  register_fixture :a_basic_user do
     User.new
   end
-  register :a_basic_blog do
+  register_fixture :a_basic_blog do
     Blog.new
   end
 
